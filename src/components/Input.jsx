94c880a48dcc03a1755b657cf3756330088e0e61
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2024-07-01 17:36:21 +0530

    login , sign up bug fixed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -39,12 +39,12 @@ const Input = forwardRef(({ className, type, ...props }, ref) => {
       onMouseMove={handleMouseMove}
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
-      className="p-[2px] rounded-lg transition duration-300 group/input w-fullz"
+      className="p-[2px] rounded-lg transition duration-300 group/input w-full"
     >
       <input
         type={type}
         className={cn(
-          `flex h-10 w-full  bg-black1 border border-gray-50/10  dark:bg-zinc-800 text-white dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
+          `flex h-10 w-full bg-black1 border border-gray-50/10  dark:bg-zinc-800 text-white dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
           file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
           focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
           disabled:cursor-not-allowed disabled:opacity-50
